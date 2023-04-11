16eb40eb882623a6e50c58c2d6edfd71668c0513
IanNene <iannene99@gmail.com>
2023-04-11 07:36:27 -0700

    added comments

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,8 @@
  */
 		
 char *create_array(unsigned int size, char c)
+// Returns a pointer to an array of 'size' chars, allocated dynamically using 'malloc'.
+// The caller is responsible for freeing the memory using 'free'.
 		
 {
 		
